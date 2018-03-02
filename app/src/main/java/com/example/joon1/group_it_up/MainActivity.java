c9e450df0de4cf4685c96dd0001ba67854d4f5b9
jchun1013 <joon1032@gmail.com>
2018-03-02 16:43:53 -0500

    start

---
+++ app/src/main/java/com/example/joon1/group_it_up/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.joon1.group_it_up;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
