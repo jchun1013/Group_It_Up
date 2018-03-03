c69e4942e7869784cc9d39a880afffc7444ae809
Steven Yuh <stevenyuh96@gmail.com>
2018-03-02 23:35:15 -0500

    added dropdown

--- app/src/main/java/com/example/joon1/group_it_up/Controller/Make_EventCard_Page.java
+++ app/src/main/java/com/example/joon1/group_it_up/Controller/Make_EventCard_Page.java
@@ -8,19 +8,26 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 
+import com.example.joon1.group_it_up.Model.Experience;
+import com.example.joon1.group_it_up.Model.Gender;
 import com.example.joon1.group_it_up.Model.Sports;
 import com.example.joon1.group_it_up.R;
 
 public class Make_EventCard_Page extends AppCompatActivity {
 
-    private Spinner sportSpinner;
+    private Spinner sportSpinner, genderSpinner, expSpinner;
     private Button mapButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_make__even_card__page);
 
-
+        genderSpinner = (Spinner) findViewById(R.id.genderSpinner);
+        expSpinner = (Spinner) findViewById(R.id.expSpinner);
+        sportSpinner = (Spinner) findViewById(R.id.sportSpinner);
+        sportSpinner.setAdapter(new ArrayAdapter<Sports>(this, android.R.layout.simple_spinner_item, Sports.values()));
+        genderSpinner.setAdapter(new ArrayAdapter<Gender>(this, android.R.layout.simple_spinner_item, Gender.values()));
+        expSpinner.setAdapter(new ArrayAdapter<Experience>(this, android.R.layout.simple_spinner_item, Experience.values()));
         mapButton = (Button)findViewById(R.id.mapButton);
         mapButton.setOnClickListener(new View.OnClickListener() {
             @Override
