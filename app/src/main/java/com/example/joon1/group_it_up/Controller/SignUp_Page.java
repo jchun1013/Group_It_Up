fd967b1b455fed2bca47e8c815382c381fd9a699
mjo30 <mjo30@gatech.edu>
2018-03-02 20:06:53 -0500

    min
    min

--- app/src/main/java/com/example/joon1/group_it_up/Controller/SignUp_Page.java
+++ app/src/main/java/com/example/joon1/group_it_up/Controller/SignUp_Page.java
@@ -4,8 +4,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.joon1.group_it_up.R;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class SignUp_Page extends AppCompatActivity {
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
