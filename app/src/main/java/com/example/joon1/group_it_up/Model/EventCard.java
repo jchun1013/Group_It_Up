dcf9cdb522d0a3a724149ff7da5bde3770bb90c0
mjo30 <mjo30@gatech.edu>
2018-03-02 19:26:48 -0500

    MIN
    MIN

---
+++ app/src/main/java/com/example/joon1/group_it_up/Model/EventCard.java
@@ -0,0 +1,10 @@
+package com.example.joon1.group_it_up.Model;
+
+/**
+ * Created by Minky on 2018-03-02.
+ */
+
+public class EventCard {
+    private String title, comments;
+    private int uid, contact;
+}
--- app/src/main/java/com/example/joon1/group_it_up/Model/User.java
+++ app/src/main/java/com/example/joon1/group_it_up/Model/User.java
@@ -6,8 +6,46 @@ package com.example.joon1.group_it_up.Model;
 
 public class User {
     private int uid;
+    private String account;
+    private String password;
+    private int contact;
 
     public User(int uid, String account, String password, int contact) {
+        this.uid = uid++;
+        this.account = account;
+        this.password = password;
+        this.contact = contact;
+    }
 
+    public int getUid() {
+        return uid;
+    }
+
+    public int getContact() {
+        return contact;
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setUid(int uid) {
+        this.uid = uid;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setContact(int contact) {
+        this.contact = contact;
     }
 }
