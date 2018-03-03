15d00b2831cceadbdeb9bd407202ff8ceaa02611
Steven Yuh <stevenyuh96@gmail.com>
2018-03-02 21:05:14 -0500

    added addEvent button

--- app/src/main/java/com/example/joon1/group_it_up/Controller/Main_List_Page.java
+++ app/src/main/java/com/example/joon1/group_it_up/Controller/Main_List_Page.java
@@ -1,16 +1,29 @@
 package com.example.joon1.group_it_up.Controller;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.joon1.group_it_up.R;
 
 public class Main_List_Page extends AppCompatActivity {
 
+    Button addBtn;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        addBtn = (Button) findViewById(R.id.addEvent);
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), Make_EventCard_Page.class));
+            }
+        });
     }
 
     public static class Registration_Page extends AppCompatActivity {
